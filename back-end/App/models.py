e590f51f2d4a9ec73f06bdb1ce02a14b0fb0cccd
Zhas Serik <zhasserik2019@gmail.com>
2025-08-16 11:46:30 +0500

    Combined front-end and back-end

--- back-end/App/models.py
+++ back-end/App/models.py
@@ -5,27 +5,33 @@ from django.conf import settings
 from django.contrib.auth.models import BaseUserManager
 from django.utils import timezone
 from datetime import timedelta
+from django.db.models import UniqueConstraint
 
 
-
+class Facility(models.Model):
+    name = models.CharField(max_length=20,null=True,blank=True)
+    members_num = models.IntegerField(default=1,null=True,blank=True)
+  
 class User(AbstractUser):
     ROLE_CHOICES = [
-        ('worker','Сотрудник'),
-        ('accountant','Бухгалтер'),
-        ('hr','Управляющий'),
-        ('president','Президент')
-        
+        ('user','Пользователь'),
+        ('admin','Админ')  
     ]
-    FIO = models.CharField(max_length=255,blank=True,null=True)
     email = models.EmailField(unique=True,blank=True,null=True)
+    facility = models.ForeignKey(Facility,on_delete=models.CASCADE,related_name="facility_membership")
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-    social_score = models.IntegerField(default=0, blank=True, null=True)
     is_admin = models.BooleanField(default=False)
-    vacation_days_left = models.IntegerField(default=2, blank=True, null=True)
-    points = models.IntegerField(default = 0)
     
+class Wallet(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='user_wallet')
+    facility = models.ForeignKey(Facility, on_delete=models.CASCADE)
+    points = models.IntegerField(default = 0)
+    updated_at = models.DateField(auto_now=True)
+    class Meta:
+        constraints = [UniqueConstraint(fields=['user','facility'], name='uniq_wallet')]
+
 class Sendcode(models.Model):
     email = models.EmailField()
     code = models.CharField(max_length=5)
@@ -33,116 +39,7 @@ class Sendcode(models.Model):
     is_used = models.BooleanField(default=False)
 
     def is_expired(self):
-        return timezone.now() > self.created_at + timedelta(minutes=10)
-
-
-class Document(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    vacation_start_date = models.DateField()
-    vacation_end_date = models.DateField()
-    create_date = models.DateField(auto_now_add=True)
-    attached_file = models.FileField(upload_to='document/',blank=True,null=True)
-    signature = models.TextField(blank=True, null=True)  # base64-подпись
-    signed_at = models.DateTimeField(blank=True, null=True)
-    
-
-    STATUS_CHOICES = [
-        ('draft', 'Черновик'),
-        ('waiting', 'Ожидает'),
-        ('approved', 'Одобрено'),
-        ('rejected', 'Отклонено'),
-    ]
-    DOC_CHOICES = [
-        ('statement', 'Заявление'),
-        ('explanatory_note', 'Объяснительная'),
-        ('travel_assignment', 'Командировка'),
-        ('act', 'Акт'),
-        ('order', 'Приказ'),
-        ('protocol', 'Протокол'),
-        ('reference', 'Справка'),
-        ('vacation', 'Отпуск'),
-    ]
-    doc_type = models.CharField(max_length=30, choices=DOC_CHOICES)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='draft')
-    @property
-    def vacation_days(self):
-        if self.vacation_start_date and self.vacation_end_date:
-            return (self.vacation_end_date - self.vacation_start_date).days + 1
-        return 0
-    
-    def __str__(self):
-        return f"{self.author} ({self.doc_type})"
-    
-class DocumentHistory(models.Model):
-    document = models.ForeignKey(Document,on_delete=models.CASCADE,related_name='history')
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    action = models.CharField(max_length=190)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    comment = models.TextField(blank=True,null=True)
-
-    def __str__(self):
-        return f"{self.user} - {self.action} - {self.timestamp} - {self.comment}"
-    
-class ApprovalStep(models.Model):
-    STATUS_CHOICES = [
-        ('waiting', 'Ожидает'),
-        ('approved', 'Подписано'),
-        ('rejected', 'Отклонено'),
-    ]
-
-    document = models.ForeignKey(Document,on_delete=models.CASCADE,related_name='approval_steps')
-    approver = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    order = models.IntegerField(blank=True,null=True)
-    status = models.CharField(max_length=20,choices=STATUS_CHOICES,default='waiting')
-    approved_at = models.DateTimeField(blank=True,null=True)
-    comment = models.TextField(max_length=255,blank=True,null=True)
-    def __str__(self):
-        return f"{self.document} ({self.approver}), {self.order}, {self.status}, {self.approved_at},{self.comment}"
-
-
-class Notification(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    document = models.ForeignKey(Document, on_delete=models.CASCADE)
-    message = models.TextField()
-    is_read = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Notification for {self.user} - {self.document} - {self.message[:20]} - {'Read' if self.is_read else 'Unread'}"
-    
-
-    def get_absolute_url(self):
-        return reverse('notification_detail', kwargs={'pk': self.pk})
-    def mark_as_read(self):
-        self.is_read = True
-        self.save()
-    def mark_as_unread(self):
-        self.is_read = False
-        self.save()
-    def get_document_url(self):
-        return reverse('document_detail', kwargs={'pk': self.document.pk})
-    def get_user_url(self):
-        return reverse('user_detail', kwargs={'pk': self.user.pk})
-    def get_notification_url(self):
-        return reverse('notification_detail', kwargs={'pk': self.pk})
-    def get_notification_list_url(self):
-        return reverse('notification_list')
-    def get_document_list_url(self):
-        return reverse('document_list')
-    def get_user_list_url(self):
-        return reverse('user_list')
-    
-class ScoreTransaction(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='score_transactions')
-    points = models.IntegerField()
-    description = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def str(self):
-        return f"{self.user} - {self.points} points - {self.description[:20]} - {self.created_at}"
-
-    class Meta:
-        ordering = ['-created_at']
+        return timezone.now() > self.created_at + timedelta(minutes=10)  
 
 class Rooms(models.Model):
     number = models.IntegerField(unique=True)
@@ -154,6 +51,9 @@ class Report(models.Model):
     REPORT_TYPES = [
         ('computer', 'Computer is broken'),
         ('light', 'Light Issue'),
+        ('water','Water/plumbing'),
+        ('internet','Wifi/Internet'),
+        ('heating','Heating/Cooling'),
         ('other', 'Other'),
     ]
     STATUS_TYPES = [
@@ -167,6 +67,8 @@ class Report(models.Model):
     status = models.CharField(max_length=50,choices=STATUS_TYPES,default='Pending')
     attachment = models.FileField(upload_to='documents/',null=True,blank=True)
     user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='reports', null=True, blank=True)
+    user_name = models.CharField(max_length=25)
+    report_date = models.DateTimeField(auto_now_add=True)
     def save(self, *args, **kwargs):
         if not self.pk and self.user:
             self.user.points = (self.user.points or 0) +1 
@@ -181,7 +83,6 @@ class Product(models.Model):
     def __str__(self):
         return self.label
     
-
 class Redeem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
--- back-end/App/serializers.py
+++ back-end/App/serializers.py
@@ -6,63 +6,19 @@ from django.contrib.auth import get_user_model
 import random, string
 from datetime import timedelta
 
-class DocumentSerializer(serializers.ModelSerializer):
-    author = serializers.SerializerMethodField()
-    class Meta:
-        model = Document
-        fields = '__all__'
-    read_only_fields = ['id', 'create_date', 'signed_at', 'vacation_days','author']
-    vacation_start_date = serializers.DateField(
-    input_formats=['%Y-%m-%d', '%d.%m.%Y', '%d/%m/%Y']
-    )
-    vacation_end_date = serializers.DateField(
-        input_formats=['%Y-%m-%d', '%d.%m.%Y', '%d/%m/%Y']
-    )
-    
-    
-    def get_author(self, obj):
-        return obj.author.username if obj.author else None
-    def create(self, validated_data):
-        user = self.context['request'].user
-        if validated_data.get('vacation_start_date') and validated_data.get('vacation_end_date'):
-            if validated_data['vacation_start_date'] > validated_data['vacation_end_date']:
-                raise serializers.ValidationError("Дата начала отпуска не может быть позже даты окончания отпуска.")
-
-            else:
-                num_days = (validated_data['vacation_end_date'] - validated_data['vacation_start_date']).days + 1
-                
-                if user.vacation_days_left < num_days:
-                    raise serializers.ValidationError(f"Недостаточно выходных дней. Осталось: {user.vacation_days_left}, нужно: {num_days}")
-                validated_data['author'] = user
-                return super().create(validated_data)
-
-
-class DocumentHistorySer(serializers.ModelSerializer):
-    class Meta:
-        model = DocumentHistory
-        fields = '__all__'
-
-
-class ApprovalStepSer(serializers.ModelSerializer):
-    class Meta:
-        model = ApprovalStep
-        fields = '__all__'
-
-
-class NotificationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notification
-        fields = '__all__'
-        read_only_fields = ['is_read', 'created_at']
     
     
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'username', 'password', 'FIO', 'role', 'social_score', 'is_admin', 'vacation_days_left']
+        fields = ['id', 'email', 'username', 'password', 'role', 'is_admin', 'facility']
 
-    
-    
+class WalletSerializer(serializers.ModelSerializer):
+    facility_name = serializers.CharField(source='facility.name', read_only=True)
+    class Meta:
+        model = Wallet
+        fields = '__all__'
+        read_only_fields = ['user','facility','facility_name']
 
 class SendCodeSerializer(serializers.Serializer):
 
@@ -127,21 +83,16 @@ class RoomSerializer(serializers.ModelSerializer):
         fields = ['id', 'number']
 
 class ReportSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Report
         fields = '__all__'
-        read_only_fields = ['user']
+        read_only_fields = ['user','user_name']
 
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
+        validated_data['user_name'] = self.context['request'].user.username
         return super().create(validated_data)
-
-class ScoreTransactionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ScoreTransaction
-        fields = ['id', 'user', 'points', 'description']
-        read_only_fields = ['id']
-
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -152,3 +103,8 @@ class RedeemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Redeem
         field = ['user', 'product', 'redeem_date', 'code']
+
+class FacilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Facility
+        fields = '__all__'
